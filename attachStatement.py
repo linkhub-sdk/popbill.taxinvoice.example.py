96798cb8b046b4ea7fe2f2b5e3551917f50b98e4
JohnJeong123 <frenchofkiss@gmail.com>
2016-07-29 09:52:48 +0900

    added Attach/Detach Statement API

---
+++ attachStatement.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+imp.reload(sys)
+try: sys.setdefaultencoding('UTF8')
+except Exception as E: pass
+
+import testValue
+
+from popbill import TaxinvoiceService,PopbillException
+
+taxinvoiceService =  TaxinvoiceService(testValue.LinkID,testValue.SecretKey)
+taxinvoiceService.IsTest = testValue.IsTest
+
+try:
+    print( "=" * 15 + " 전자명세서 첨부 " + "=" * 15)
+
+    MgtKeyType = "SELL"        # 세금계산서 유형 , SELL : 매출 , BUY : 매입 , TRUSTEE : 수탁
+    MgtKey = "20160728-03"     # 세금계산서 문서관리번호
+    ItemCode = "121"           # 전자명세서 종류코드, 121-명세서, 122-청구서, 123-견적서, 124-발주서 125-입금표, 126-영수증
+    StmtMgtKey = "fbrdavxpsn"  # 전자명세서 문서관리번호
+
+    result = taxinvoiceService.attachStatement(testValue.testCorpNum, MgtKeyType, MgtKey, ItemCode, StmtMgtKey, testValue.testUserID)
+
+    print("처리결과 : [%d] %s" % (result.code,result.message))
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code , PE.message))
