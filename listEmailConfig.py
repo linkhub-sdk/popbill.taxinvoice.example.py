65c0fc2a9c8b2cbcc8ca4390f570eaede0cd5195
kimhyunjin <khjzzm@gmail.com>
2018-09-12 10:25:19 +0900

    listEmailConfig print format change

--- listEmailConfig.py
+++ listEmailConfig.py
@@ -33,57 +33,57 @@ try:
 
     for info in EmailConfig:
         if info.emailType == "TAX_ISSUE":
-            print info.emailType, "(공급받는자에게 전자세금계산서 발행 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 전자세금계산서 발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_ISSUE_INVOICER":
-            print info.emailType, "(공급자에게 전자세금계산서 발행 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 전자세금계산서 발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_CHECK":
-            print info.emailType, "(공급자에게 전자세금계산서 수신확인 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 전자세금계산서 수신확인 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_CANCEL_ISSUE":
-            print info.emailType, "(공급받는자에게 전자세금계산서 발행취소 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 전자세금계산서 발행취소 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_SEND":
-            print info.emailType, "(공급받는자에게 [발행예정] 세금계산서 발송 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 [발행예정] 세금계산서 발송 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_ACCEPT":
-            print info.emailType, "(공급자에게 [발행예정] 세금계산서 승인 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 [발행예정] 세금계산서 승인 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_ACCEPT_ISSUE":
-            print info.emailType, "(공급자에게 [발행예정] 세금계산서 자동발행 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 [발행예정] 세금계산서 자동발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_DENY":
-            print info.emailType, "(공급자에게 [발행예정] 세금계산서 거부 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 [발행예정] 세금계산서 거부 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_CANCEL_SEND":
-            print info.emailType, "(공급받는자에게 [발행예정] 세금계산서 취소 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 [발행예정] 세금계산서 취소 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_REQUEST":
-            print info.emailType, "(공급자에게 세금계산서를 발행요청 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 세금계산서를 발행요청 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_CANCEL_REQUEST":
-            print info.emailType, "(공급받는자에게 세금계산서 취소 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 세금계산서 취소 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_REFUSE":
-            print info.emailType, "(공급받는자에게 세금계산서 거부 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 세금계산서 거부 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_ISSUE":
-            print info.emailType, "(공급받는자에게 전자세금계산서 발행 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 전자세금계산서 발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_ISSUE_TRUSTEE":
-            print info.emailType, "(수탁자에게 전자세금계산서 발행 메일 전송 여부) :", info.sendYN
+            print("%s(수탁자에게 전자세금계산서 발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_ISSUE_INVOICER":
-            print info.emailType, "(공급자에게 전자세금계산서 발행 메일 전송 여부) :", info.sendYN
+            print("%s(공급자에게 전자세금계산서 발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_CANCEL_ISSUE":
-            print info.emailType, "(공급받는자에게 전자세금계산서 발행취소 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 전자세금계산서 발행취소 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_CANCEL_ISSUE_INVOICER":
-            print info.emailType, "(공급자에게 전자세금계산서 발행취소 메) :", info.sendYN
+            print("%s(공급자에게 전자세금계산서 발행취소 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_SEND":
-            print info.emailType, "(공급받는자에게 [발행예정] 세금계산서 발송 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 [발행예정] 세금계산서 발송 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_ACCEPT":
-            print info.emailType, "(수탁자에게 [발행예정] 세금계산서 승인 메일 전송 여부) :", info.sendYN
+            print("%s(수탁자에게 [발행예정] 세금계산서 승인 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_ACCEPT_ISSUE":
-            print info.emailType, "(수탁자에게 [발행예정] 세금계산서 자동발행 메일 전송 여부) :", info.sendYN
+            print("%s(수탁자에게 [발행예정] 세금계산서 자동발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_DENY":
-            print info.emailType, "(수탁자에게 [발행예정] 세금계산서 거부 메일 전송 여부) :", info.sendYN
+            print("%s(수탁자에게 [발행예정] 세금계산서 거부 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_TRUST_CANCEL_SEND":
-            print info.emailType, "(공급받는자에게 [발행예정] 세금계산서 취소 메일 전송 여부) :", info.sendYN
+            print("%s(공급받는자에게 [발행예정] 세금계산서 취소 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_CLOSEDOWN":
-            print info.emailType, "(거래처의 휴폐업 여부 확인 메일 전송 여부) :", info.sendYN
+            print("%s(거래처의 휴폐업 여부 확인 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_NTSFAIL_INVOICER":
-            print info.emailType, "(전자세금계산서 국세청 전송실패 안내 메일 전송 여부) :", info.sendYN
+            print("%s(전자세금계산서 국세청 전송실패 안내 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "TAX_SEND_INFO":
-            print info.emailType, "(전월 귀속분 [매출 발행 대기] 세금계산서 발행 메일 전송 여부) :", info.sendYN
+            print("%s(전월 귀속분 [매출 발행 대기] 세금계산서 발행 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
         if info.emailType == "ETC_CERT_EXPIRATION":
-            print info.emailType, "(팝빌에서 이용중인 공인인증서의 갱신 메일 전송 여부) :", info.sendYN
+            print("%s(팝빌에서 이용중인 공인인증서의 갱신 메일 전송 여부) : %s" % (info.emailType, info.sendYN))
 
 
 except PopbillException as PE:
