b8952bbd37285c3634d09615074a476e13d34a83
1jae <wjkim@linkhub.co.kr>
2021-04-29 11:02:56 +0900

    added getContactInfo, getSendToNTSConfig API

---
+++ getSendToNTSConfig.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+import testValue
+
+from popbill import TaxinvoiceService, PopbillException
+
+taxinvoiceService = TaxinvoiceService(testValue.LinkID, testValue.SecretKey)
+taxinvoiceService.IsTest = testValue.IsTest
+taxinvoiceService.IPRestrictOnOff = testValue.IPRestrictOnOff
+taxinvoiceService.UseStaticIP = testValue.UseStaticIP
+taxinvoiceService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+국세청 전송 설정 확인
+- https://docs.popbill.com/taxinvoice/python/api#GetSendToNTSConfig
+'''
+
+try:
+    print("=" * 15 + " 국세청 전송 설정 확인 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    sendToNTSConfig = taxinvoiceService.getSendToNTSConfig(CorpNum)
+
+    print("sendToNTS: %s" % sendToNTSConfig.sendToNTS)
+
+except PopbillException as PE:
+    print("Popbill Exception : [%d] %s" % (PE.code, PE.message))
