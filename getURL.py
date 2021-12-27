3753c4eaafdcbe5caa24f5b201946b9979d231bc
ProWON <pro_won45@naver.com>
2021-12-27 16:38:24 +0900

    fixed comment url function

--- getURL.py
+++ getURL.py
@@ -21,7 +21,7 @@ taxinvoiceService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 '''
 팝빌 전자세금계산서 관련 문서함 팝업 URL을 반환합니다.
-- 보안정책으로 인해 반환된 URL의 유효시간은 30초입니다.
+- 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
 - https://docs.popbill.com/taxinvoice/python/api#GetURL
 '''
 
